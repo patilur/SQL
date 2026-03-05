606932c6fee1723ef111e42c8ba85cd34dc5279d
patilur <utkarshapatil0145@gmail.com>
2026-03-05 14:26:51 +0530

    Sequelize

---
+++ Sequalize/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const db = require('./utils/db-connection')
+const app = express();
+const studentRoutes = require('./routes/studentRoutes')
+const studentModel = require('./model/student');
+// Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.use('/students', studentRoutes);
+
+
+// 404 Handler (ALWAYS LAST)
+app.use((req, res) => {
+    res.status(404).send("Page not found");
+});
+
+db.sync({ force: false }).then(() => {
+    app.listen(3000, (err) => {
+        console.log('Server running')
+    })
+}).catch((err) => {
+    console.log(err);
+})
+
