741c4ee129f4db2ac70004b8af844dfdf2e05ffb
patilur <utkarshapatil0145@gmail.com>
2026-03-16 22:39:00 +0530

    leaderboard optimization

--- ExpenseApplication/controller/leaderBoardController.js
+++ ExpenseApplication/controller/leaderBoardController.js
@@ -5,22 +5,19 @@ const sequelize = require('../utils/db-connection');
 const getLeaderboard = async (req, res) => {
 
     try {
-
         if (!req.user.isPremiumUser) {
-
             return res.status(403).json({
                 message: "User is not a premium member"
             });
-
         }
-
+        //SELECT Users.id,Users.name,COALESCE(SUM(Expenses.expenseamount),0) AS totalExpens FROM Users
+        //LEFT JOIN Expenses ON Users.id = Expenses.userId
+        //GROUP BY Users.id
+        //ORDER BY totalExpense DESC;
         const leaderboard = await User.findAll({
-
             attributes: [
-
                 'id',
                 'name',
-
                 [
                     sequelize.fn(
                         'COALESCE',
@@ -29,9 +26,7 @@ const getLeaderboard = async (req, res) => {
                     ),
                     'totalExpense'
                 ]
-
             ],
-
             include: [
                 {
                     model: Expense,
@@ -39,25 +34,17 @@ const getLeaderboard = async (req, res) => {
                     required: false
                 }
             ],
-
             group: ['Users.id'],
-
             order: [[sequelize.literal('totalExpense'), 'DESC']]
-
         });
-
         res.status(200).json(leaderboard);
 
     } catch (err) {
-
         console.log(err);
-
         res.status(500).json({
             message: "Error fetching leaderboard"
         });
-
     }
-
 };
 
 module.exports = { getLeaderboard };
